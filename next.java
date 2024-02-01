a4a924e705ca2904342e731ea12020fa833b4e6a
sunney1 <sunney.kumar@nextuple.com>
2024-02-01 15:00:01 +0530

    commintin main

---
+++ next.java
@@ -0,0 +1,3 @@
+public class next {
+    just doing fo rcheckingn.
+}
--- sunney.java
+++ sunney.java
@@ -3,5 +3,5 @@ public class sunney {
     System.out.println('hello');
     dncalksnclds;
     joker hun
-
+    sunney i son develkopemnt;
 }
